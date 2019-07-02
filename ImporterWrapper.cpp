7c3fbc5a0c7cc6e75c22c9619a17ef2343d13467
Philippe Cuvillier <philippe.cuvillier@antescofo.com>
2019-07-02 20:30:18 +0200

    Incremented version to 0.2.9.

--- ImporterWrapper.cpp
+++ ImporterWrapper.cpp
@@ -54,7 +54,7 @@ ImporterWrapper::~ImporterWrapper()
 
 std::string ImporterWrapper::getVersion()
 {
-    return "version 0.2.8";
+    return "version 0.2.9";
 }
 
 bool ImporterWrapper::parseArguments( vector<string>& args )
